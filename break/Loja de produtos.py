be5533729f0756777a608fc32559d6eb6ad9e7b7
carolinerios <96029796+carolinerios@users.noreply.github.com>
2021-12-19 15:57:52 -0300

    verificação de produto mais barato

--- break/Loja de produtos.py
+++ break/Loja de produtos.py
@@ -3,9 +3,11 @@ while True:
     nom_prod =  str(input('Qual o produto o comprado? '))
     pre_prod = float(input('Qual o preço do produto ocmprado? '))
     som_pre = pre_prod + som_pre
+    c = c+1
     if pre_prod >1000:
         c_prod = c_prod + 1
-    caro = barato = pre_prod 
+    if c == 1:
+        caro = barato = pre_prod 
     if pre_prod > caro:
         caro = pre_prod
         pro_caro = nom_prod
