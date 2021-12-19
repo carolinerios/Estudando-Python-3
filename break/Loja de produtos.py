0ede70e2429f979e67b1d7c84884d3485be63107
carolinerios <96029796+carolinerios@users.noreply.github.com>
2021-12-19 15:41:22 -0300

    Loja de Produtos Criado

---
+++ break/Loja de produtos.py
@@ -0,0 +1,21 @@
+som_pre = c_prod = 0
+while True:
+    nom_prod =  str(input('Qual o produto o comprado? '))
+    pre_prod = float(input('Qual o preço do produto ocmprado? '))
+    som_pre = pre_prod + som_pre
+    if pre_prod >1000:
+        c_prod = c_prod + 1
+    caro = barato = pre_prod 
+    if pre_prod > caro:
+        caro = pre_prod
+        pro_caro = nom_prod
+    if pre_prod < barato:
+        barato = pre_prod
+        pro_barato = nom_prod
+    end = str(input('Encerrar compra? Digite S/N ')).strip().upper()[0]
+    if end in 'S':
+        break
+print(f'A soma da compra foi de R$ {som_pre}')
+print(f'{c_prod} produto(s) comprado(s) custa(m) mais de R$1.000')
+print(f'O produto mais barato foi {pro_barato}')
+
