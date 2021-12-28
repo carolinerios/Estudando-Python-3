1e3a1c00070ff7ded7788581ad797b37bd3c065f
carolinerios <96029796+carolinerios@users.noreply.github.com>
2021-12-28 14:33:49 -0300

    1ª versão exercício 073 (escrevendo itens da tuplas em relação a sua posição)

---
+++ tuplas/escrevendo números por extenso.py
@@ -0,0 +1,21 @@
+# Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+acabou = False #definindo condição para laço
+enc = ' '
+num_ext = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze','treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+while not acabou:
+    num = int(input('Digite um número entre 0-20 para vê-lo escrito por extenso: '))
+    while num > 21:
+        if num > 21:
+            print('Você digitou um número maior que 20')
+            num = int(input('Digite um número entre 0-20 para vê-lo escrito por extenso: '))
+    esc = num_ext[num] # extraindo a posição específica da tupla
+    print(f'O número {num} por extenso é {esc}')
+    while enc not in 'SN':
+        enc = str(input('Você deseja ver mais números por extenso? Responda S/N' )).strip().upper()
+    if enc == 'S':
+        enc = ' '
+    else:
+        acabou = True
+print('='*20)
+print('FIM')
