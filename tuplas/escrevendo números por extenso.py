9ca2cbc9f88e68cf3da2e08e3b13990a39ddd1e0
Perkles <caroline.rios.al@gmail.com>
2022-01-07 16:15:24 -0300

    Linhas de verificação de números menores que zero

--- tuplas/escrevendo números por extenso.py
+++ tuplas/escrevendo números por extenso.py
@@ -5,9 +5,12 @@ enc = ' '
 num_ext = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze','treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
 while not acabou:
     num = int(input('Digite um número entre 0-20 para vê-lo escrito por extenso: '))
-    while num > 21:
+    while num > 21 or num < 0:
         if num > 21:
             print('Você digitou um número maior que 20')
+            num = int(input('Digite um número entre 0-20 para vê-lo escrito por extenso: '))
+        if num < 0:
+            print('Você digitou um número menor que o')
             num = int(input('Digite um número entre 0-20 para vê-lo escrito por extenso: '))
     esc = num_ext[num] # extraindo a posição específica da tupla
     print(f'O número {num} por extenso é {esc}')
