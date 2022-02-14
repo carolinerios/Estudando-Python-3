298d00ce72f0c2dca59f22a3766cf6e90f7c871a
Perkles <caroline.rios.al@gmail.com>
2022-02-14 17:35:25 -0300

    Início da escrita da lógica

---
+++ tuplas/Vogais em tuplas.py
@@ -0,0 +1,14 @@
+#Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+banco_pal = ("caroline", "historia", "python", "curiosidade", "tecnologia", "ciencia", "analise", "dados")
+print("><  "*51)
+print("ANALISADOR DE PALAVRAS")
+for pal in range(0, len(banco_pal)):
+    print(f'A palavra "{banco_pal[pal]}" tem as vogais: ', end = '')
+    #for vog in range(0, len(banco_pal[pal]),1:
+        #print
+    if 'aeiou' in banco_pal[pal]:
+        print(f{})
+
+    #and 'e' and 'i' and 'o' and 'u' in banco_pal[c]:
+        print(f'')
+
