0933de2d0a895d24720a303311c7cd2509e634b8
Perkles <caroline.rios.al@gmail.com>
2022-02-15 12:48:15 -0300

    versão funcional

--- tuplas/Vogais em tuplas.py
+++ tuplas/Vogais em tuplas.py
@@ -1,14 +1,11 @@
 #Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
-banco_pal = ("caroline", "historia", "python", "curiosidade", "tecnologia", "ciencia", "analise", "dados")
-print("><  "*51)
+banco_pal = ("caroline", "Historia", "Python", "curiosidade", "tecnologia", "ciencia", "analise", "dados")
+print("><  "*33)
 print("ANALISADOR DE PALAVRAS")
-for pal in range(0, len(banco_pal)):
-    print(f'A palavra "{banco_pal[pal]}" tem as vogais: ', end = '')
-    #for vog in range(0, len(banco_pal[pal]),1:
-        #print
-    if 'aeiou' in banco_pal[pal]:
-        print(f{})
+for pal in banco_pal: #
+    print(f'\n A palavra "{pal}" tem as vogais: ', end = '')
+    for let in pal:
+        if let.lower() in 'aeiou':
+            print(f'{let}', end = '')
 
-    #and 'e' and 'i' and 'o' and 'u' in banco_pal[c]:
-        print(f'')
 
